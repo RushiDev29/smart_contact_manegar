870d3b9b2796e2c24852d0214bddcbb9cf888117
RishiDev_29 <144615033+RushiDev29@users.noreply.github.com>
2024-09-05 14:36:10 +0530

    service file

---
+++ backend/src/main/java/com/scm/Services/UserService.java
@@ -0,0 +1,15 @@
+package com.smart.service;
+
+import com.smart.entity.User;
+
+public interface UserService {
+	
+	// save user method..
+	public User saveUser(User data);
+	
+	//public boolean checkUser(String emailId , String password);
+	
+	
+	
+
+}
