b5389f71f56c7c60a6bc7c4a98ba4c9b474af57b
RishiDev_29 <144615033+RushiDev29@users.noreply.github.com>
2024-09-05 14:14:21 +0530

    Delete src/main directory

--- src/main/java/com/scm/Repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smart.entity.Contact;
-
-public interface ContactRepository  extends JpaRepository<Contact, Integer>{
-
-}
