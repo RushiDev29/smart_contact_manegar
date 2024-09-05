db3d0a388c4a1dc5d281c8d59ea61d67458b40bb
RishiDev_29 <144615033+RushiDev29@users.noreply.github.com>
2024-09-05 13:08:46 +0530

    Add files via upload

---
+++ frontend/src/app/componants/main-app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from 'express';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'smartcontactmanegar';
+  val=0;
+
+ 
+  
+
+
+
+}
