823d680d51d65cc33c98db8c3509be456fe2cb29
RishiDev_29 <144615033+RushiDev29@users.noreply.github.com>
2024-09-05 13:05:21 +0530

    Add files via upload

---
+++ frontend/src/app/componants/user-dashbord/update-contact/update-contact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-contact',
+  templateUrl: './update-contact.component.html',
+  styleUrl: './update-contact.component.css'
+})
+export class UpdateContactComponent {
+
+}
